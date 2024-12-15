359638e149148a3394949d2b5c7900878bfbe81e
YAMIPC <srv013@gmail.com>
2024-12-15 09:37:17 -0300

    15.12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+// @ts-check
+import { defineConfig } from 'astro/config';
 
-import robotsTxt from "astro-robots-txt"
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  integrations: [tailwind()]
+});
